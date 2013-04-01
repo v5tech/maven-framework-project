3c512535291a542a6e9c23ccb42f10a72ac2911b
sxyx2008 <sxyx2008@163.com>
2013-04-01 13:46:22 +0800

    新增Spring MVC 返回pdf视图

---
+++ src/main/java/com/fengjing/framework/springmvc/controller/iTextRevenueReportController.java
@@ -0,0 +1,44 @@
+package com.fengjing.framework.springmvc.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.ServletRequestUtils;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.AbstractController;
+
+@Controller
+public class iTextRevenueReportController extends AbstractController {
+
+	@Override
+	@RequestMapping("/itext")
+	protected ModelAndView handleRequestInternal(HttpServletRequest request,
+			HttpServletResponse response) throws Exception {
+		String output = ServletRequestUtils.getStringParameter(request,"output");
+
+		// dummy data
+		Map<String, String> revenueData = new HashMap<String, String>();
+		revenueData.put("1/20/2010", "$100,000");
+		revenueData.put("1/21/2010", "$200,000");
+		revenueData.put("1/22/2010", "$300,000");
+		revenueData.put("1/23/2010", "$400,000");
+		revenueData.put("1/24/2010", "$500,000");
+
+		if (output == null || "".equals(output)) {
+			// return normal view
+			return new ModelAndView("RevenueSummary", "revenueData",revenueData);
+		} else if ("PDF".equals(output.toUpperCase())) {
+			// return excel view
+			return new ModelAndView("PdfRevenueSummary", "revenueData",revenueData);
+		} else {
+			// return normal view
+			return new ModelAndView("RevenueSummary", "revenueData",revenueData);
+		}
+	}
+
+}
---
+++ src/main/java/com/fengjing/framework/springmvc/itext/viewer/PdfRevenueReportView.java
@@ -0,0 +1,38 @@
+package com.fengjing.framework.springmvc.itext.viewer;
+
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.view.document.AbstractPdfView;
+
+import com.lowagie.text.Document;
+import com.lowagie.text.Table;
+import com.lowagie.text.pdf.PdfWriter;
+
+@Service("PdfRevenueSummary")
+public class PdfRevenueReportView extends AbstractPdfView {
+
+	@Override
+	protected void buildPdfDocument(Map model, Document document,
+		PdfWriter writer, HttpServletRequest request,
+		HttpServletResponse response) throws Exception {
+ 
+		@SuppressWarnings("unchecked")
+		Map<String,String> revenueData = (Map<String,String>) model.get("revenueData");
+ 
+		Table table = new Table(2);
+		table.addCell("Month");
+		table.addCell("Revenue");
+ 
+		for (Map.Entry<String, String> entry : revenueData.entrySet()) {
+			table.addCell(entry.getKey());
+			table.addCell(entry.getValue());
+                }
+ 
+		document.add(table);
+	}
+
+}
