e82779e11ed8f9e6e339b10ba92191d79919d0d2
sxyx2008 <sxyx2008@163.com>
2013-04-11 16:27:44 +0800

    Spring整合Struts

--- src/main/java/com/fengjing/framework/struts/action/HelloWorldAction.java
+++ src/main/java/com/fengjing/framework/struts/action/HelloWorldAction.java
@@ -3,14 +3,14 @@ package com.fengjing.framework.struts.action;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.struts.action.Action;
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
+import org.apache.struts.actions.MappingDispatchAction;
 
 import com.fengjing.framework.struts.form.HelloWorldForm;
 
-public class HelloWorldAction extends Action {
+public class HelloWorldAction extends MappingDispatchAction {
 	
 	public ActionForward execute(ActionMapping mapping, ActionForm form,HttpServletRequest request, HttpServletResponse response)
 			throws Exception {
