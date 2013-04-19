09baae11a9d060c5493fca6e085529e64c7c328d
sxyx2008 <sxyx2008@163.com>
2013-04-19 18:13:53 +0800

    CXF整合Sring发布WebService

--- src/main/java/com/fengjing/framework/webservice/cxf/CxfClient.java
+++ src/main/java/com/fengjing/framework/webservice/cxf/CxfClient.java
@@ -1,13 +1,20 @@
 package com.fengjing.framework.webservice.cxf;
 
-import org.junit.Test;
-import org.springframework.test.context.ContextConfiguration;
+import javax.annotation.Resource;
 
-@ContextConfiguration(locations="classpath:")
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@ContextConfiguration(locations="classpath:spring-cxf-servlet.xml")
+@RunWith(SpringJUnit4ClassRunner.class)
 public class CxfClient {
+	@Resource(name="helloClient")
+	HelloWorld helloWorld;
 	
 	@Test
 	public void test(){
-		
+		System.out.println(helloWorld.sayHi("Spring"));
 	}
 }
